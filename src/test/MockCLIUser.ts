74b87eb6e947e36858e23a1b1fca2d86201b139e
Alfred Cepeda <alfred@cepeda.io>
2022-06-04 00:09:22 -0400

    Progress

--- src/test/MockCLIUser.ts
+++ src/test/MockCLIUser.ts
@@ -1,22 +1,50 @@
 import {ChildProcess, spawn, SpawnOptions} from "child_process";
 
+function isPromptSpec(obj:any): obj is PromptSpec {
+  return typeof obj.prompt === 'string'
+    && Array.isArray(obj.willSend);
+}
+
 export interface MockCLIUserOptions extends SpawnOptions {
   output?: (output: string) => void;
 }
 
+interface TestOptions {
+  timeout?: number;
+}
+
+interface SpecController {
+  resolve: Function;
+  reject: Function;
+  timeout: number;
+  timer?: NodeJS.Timer;
+  spec: Spec;
+}
+
+interface Spec {
+  resolve(output:string): boolean;
+}
+
+interface EncounterAllSpec extends Spec {
+  prompts: string[];
+}
+
+interface WaitingForSpec extends Spec {
+  prompt: string;
+}
+
+interface PromptSpec extends Spec {
+  prompt: string;
+  willSend: string[];
+}
+
 export class MockCLIUser {
   started: boolean = false;
-  waitingFor?:string;
-  waitingForNext: boolean;
-  willSend?:string[];
-  resolve?: Function;
-  reject?: Function;
-  prompt?:string;
-  sentInput: boolean = false;
+  controller?:SpecController;
   process: ChildProcess;
-  timeout: number = 1000;
-  timer: NodeJS.Timer;
+  testTimer: NodeJS.Timer;
   options: MockCLIUserOptions;
+  specTimeout: number = 1000;
 
   constructor(
     public command: string,
@@ -53,105 +81,164 @@ export class MockCLIUser {
         this.options.output(output);
       }
 
-      if(!this.resolve) {
+      if(!this.controller) {
         return;
       }
 
-      if(this.prompt && !this.sentInput && output.includes(this.prompt)) {
-        this.sentInput = true;
-        this.willSend.forEach((chunk) => this.process.stdin.write(chunk));
-
-        if(!this.waitingFor) {
-          this.resolveSend(output)
+      if(this.controller.spec.resolve(output)) {
+        if(this.controller.timer) {
+          clearTimeout(this.controller.timer);
         }
-      }
 
-      if(this.waitingFor && output.includes(this.waitingFor)) {
-        this.resolveSend(output);
-      }
-
-      if(this.waitingForNext) {
-        this.resolveSend(output);
+        this.controller.resolve(output);
+        this.controller = null;
       }
     });
   }
 
-  private resolveSend(output: string) {
-    const resolve = this.resolve;
-    this.clear();
-    resolve(output);
-  }
-
-  private clear() {
-    if(this.timer) {
-      clearTimeout(this.timer);
-    }
-
-    this.resolve = null;
-    this.reject = null;
-    this.waitingFor = null;
-    this.waitingForNext = false;
-    this.sentInput = false;
-    this.prompt = null;
-    this.willSend = null
-    this.timer = null;
-  }
-
-  setupResolution(resolve, reject, message) {
-    this.resolve = resolve;
-    this.reject = reject;
+  setupController(controller:SpecController) {
+    this.controller = controller;
 
     if(!this.started) {
       this.start();
     }
-
-    if(this.timeout > 0) {
-      this.timer = setTimeout(() => {
-        if(this.reject) {
-          const error = message ? `Waiting for timeout reached: ${message}` : 'Waiting for timeout reached';
-          this.reject(new Error(error));
-
+    
+    if(this.controller.timeout > 0) {
+      this.controller.timer = setTimeout(() => {
+        if(this.controller) {
+          const error = isPromptSpec(this.controller.spec) ? `Waiting for timeout reached: ${this.controller.spec.prompt}` : 'Waiting for timeout reached';
+          this.controller.reject(new Error(error));
           this.process.kill();
-          this.clear();
+          this.controller = null;
         }
-      }, this.timeout);
+      }, this.controller.timeout);
     }
   }
 
-  send(prompt: string, answer?:string | string[], message?:string) {
-    this.prompt = prompt;
-    this.waitingFor = message;
+  test(tuples:Array<
+    string
+    | [string]
+    | [string, string | string[]]
+  >, options: TestOptions = {}) {
+    const clearTimer = () => {
+      if(this.testTimer) {
+        clearTimeout(this.testTimer);
+      }
 
-    if(Array.isArray(answer)) {
-      this.willSend = answer;
-    } else if(typeof answer === 'string') {
-      this.willSend = [answer];
-    } else {
-      this.willSend = [];
+      this.testTimer = null;
     }
 
-    if(answer && this.willSend[this.willSend.length - 1] !== '\x0D') {
-      this.willSend.push('\x0D');
-    }
-
-    return new Promise((resolve, reject) => this.setupResolution(resolve, reject, prompt));
-  }
-
-  test(tuples:Array<[string] | [string, string | string[]] | [string, string | string[], string]>) {
-    return tuples.reduce((tail, tuple) =>
-      tail.then(() => this.send.apply(this, tuple))
+    const tail = tuples.reduce((tail, tuple) =>
+      tail.then(() => {
+        if(typeof tuple === "string") {
+          return this.waitFor(tuple);
+        } else if(tuple.length === 1) {
+          return this.waitFor.apply(this, tuple);
+        } else if(tuple.length === 2) {
+          return this.send.apply(this, tuple)
+        }
+      })
     , Promise.resolve())
+
+    tail.then(() => this.waitTillDone());
+    tail.finally(() => clearTimer());
+
+
+    this.testTimer = setTimeout(() => {
+      if(this.process.connected) {
+        this.process.kill();
+        this.controller = null;
+        throw new Error('Test method timeout reached');
+      }
+
+      this.testTimer = null;
+    }, options.timeout || 10000);
+
+    return tail;
+  }
+
+  send(prompt: string, answer:string | string[]) {
+    const willSend = Array.isArray(answer) ? answer : [ answer ];
+
+    if(answer && willSend[willSend.length - 1] !== '\x0D') {
+      willSend.push('\x0D');
+    }
+
+    return new Promise((resolve, reject) =>
+      this.setupController({
+        resolve,
+        reject,
+        timeout: this.specTimeout,
+        spec: {
+          prompt,
+          willSend,
+          resolve(output: string): boolean {
+            console.log(output);
+            console.log('prompt:', this.prompt);
+            console.log(output.includes(this.prompt))
+            return output.includes(this.prompt);
+          }
+        } as PromptSpec,
+      })
+    );
+  }
+
+  encounterAll(prompts: string[]) {
+    return new Promise((resolve, reject) => {
+      const spec:EncounterAllSpec = {
+        prompts,
+        resolve(output: string): boolean {
+          const filteredPrompts = spec.prompts.filter((prompt) => !output.includes(prompt));
+
+          if(filteredPrompts.length !== spec.prompts.length) {
+            spec.prompts = filteredPrompts;
+          }
+
+          return spec.prompts.length === 0;
+        }
+      }
+
+      this.setupController({
+        resolve,
+        reject,
+        timeout: this.specTimeout,
+        spec
+      })
+    });
   }
 
   nextMessage(): Promise<string> {
-    this.waitingForNext = true;
-    return new Promise((resolve, reject) => this.setupResolution(resolve, reject, 'Waiting for next message'));
+    return new Promise((resolve, reject) => {
+      const spec:WaitingForSpec = {
+        prompt: '',
+        resolve: () => true
+      };
+
+      this.setupController({
+        resolve,
+        reject,
+        timeout: this.specTimeout,
+        spec
+      });
+    });
   }
 
   waitFor(prompt: string): Promise<string> {
-    this.waitingFor = prompt;
+    return new Promise((resolve, reject) => {
+      const spec:WaitingForSpec = {
+        prompt: prompt,
+        resolve(output: string): boolean {
+          return output.includes(spec.prompt);
+        }
+      }
 
-    return new Promise((resolve, reject) => this.setupResolution(resolve, reject, prompt));
+      this.setupController({
+        resolve,
+        reject,
+        timeout: this.specTimeout,
+        spec
+      })
+    });
   }
 
   waitTillDone() {
